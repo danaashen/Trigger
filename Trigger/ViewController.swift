7422c777f4e54d99cf466cefba9aed61adaac072
dakosh <230107034@sdu.edu.kz>
2026-04-09 20:44:01 +0500

    Initial Commit

---
+++ Trigger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Trigger
+//
+//  Created by Dana Ashen on 09.04.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
